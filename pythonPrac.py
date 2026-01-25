07cbc475bd81df95c0f8bceeacdd733474067440
Shivansh Pandey <yashpandey.1307@gmail.com>
2026-01-26 00:39:17 +0530

    more changes

--- pythonPrac.py
+++ pythonPrac.py
@@ -1,3 +1,3 @@
-print("Jarvis, you there, buddy?")
-print("Yes, I'm here! How can I assist you today?")
-print("Daddy's Home!!! We're back in harware mode. Run a diagnostic on the mainframe.")
+print("Getting started with Python!!")
+x=19
+print(x)
