1d1b9699131bd55043f5fb5e39be76e74889cfc6
Shivansh Pandey <yashpandey.1307@gmail.com>
2026-01-26 23:46:57 +0530

    learned about execution statements

--- practiseProb.py
+++ practiseProb.py
@@ -1,1 +1,17 @@
 #print sum
+a = 10
+b = 20
+print(f"Value of a is {a}")
+print(f"Value of b is {b}")
+print(f"the sum of a and b is {a + b}")
+
+#Expression execution
+A,B = 5, 10
+print(f"Value of A is {A}")
+print(f"Value of B is {B}")
+print()
+Txt = "*"
+print(2 * A * Txt + " " + 3 * B * Txt) #String and Numeric Values can operate together in expression using *
+
+a,b = '2', 3
+print((a + Txt + " ")*b)
