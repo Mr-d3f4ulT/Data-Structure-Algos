efd2df08cf02b89277c846214986109e48fa697c
Shivansh Pandey <yashpandey.1307@gmail.com>
2026-01-26 23:55:14 +0530

    Floor division seekha

--- practiseProb.py
+++ practiseProb.py
@@ -15,3 +15,15 @@ print(2 * A * Txt + " " + 3 * B * Txt)  #String and Numeric Values can operate t
 
 a,b = '2', 3
 print((a + Txt + " ")*b)  #String and String can operate together in expression using +
+
+C = 4
+print(A + B * C)  #Numeric Values can operate with all arithimetic operators
+print((A + B) * C)  #Parenthesis has highest precedence in expression evaluation
+
+D = 50.2
+
+print(C * D)  #Arithmetic operation between Integer and Float results in Float
+print(int(D))  #Type casting Float to Integer
+
+print(B/A)  #Division operator results in Float
+print(B//A)  #Floor Division operator results in Integer
