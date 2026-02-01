3ef94ad06b5b4de6dbf21b471e66da4e81e79359
Shivansh Pandey <yashpandey.1307@gmail.com>
2026-02-01 06:26:51 +0530

    Wrapper Class

---
+++ Java/wrapperClasses.java
@@ -0,0 +1,19 @@
+public class wrapperClasses {
+  public static void main(String[] args) {
+    //Wrapper Class = provides a way to use primitive data types (int, char, boolean, etc..) as reference data types (objects).
+    // can be used when working with collections (ex. ArrayList, HashMap, etc..)
+
+    //Primitive Data Types   // Wrapper Classes
+    // byte                  Byte
+    // short                 Short
+    // int                   Integer
+    // long                  Long
+    // float                 Float
+    // double                Double
+    // char                  Character
+    // boolean               Boolean
+
+    // Autoboxing = the automatic conversion that the Java compiler makes between the primitive data types and their corresponding object wrapper classes.
+    // Unboxing = the reverse process of converting an object of a wrapper class to its corresponding primitive data type.
+  }
+}
