161286ae69257d139fa3d8ff94ea2f6688505286
Shivansh Pandey <yashpandey.1307@gmail.com>
2026-02-07 10:03:26 +0530

    I feel it coming

--- Python/conditionalStatements.py
+++ Python/conditionalStatements.py
@@ -22,16 +22,16 @@ print("We will learn conditional statements in python")
 #         print(f"{n} is a prime number")
 
 #Program to find the largest of three numbers
-a = float(input("Enter the first number : "))
-b = float(input("Enter the second number : "))
-c = float(input("Enter the third number : "))
-if (a >= b) and (a >= c):
-    largest = a
-elif (b >= a) and (b >= c):
-    largest = b
-else:
-    largest = c
-    print(f"The largest number among {a}, {b} and {c} is {largest}")
+# a = float(input("Enter the first number : "))
+# b = float(input("Enter the second number : "))
+# c = float(input("Enter the third number : "))
+# if (a >= b) and (a >= c):
+#     largest = a
+# elif (b >= a) and (b >= c):
+#     largest = b
+# else:
+#     largest = c
+#     print(f"The largest number among {a}, {b} and {c} is {largest}")
 
 #CONDITIONAL EXPRESSION (TERNARY OPERATOR)  
 num = 5
