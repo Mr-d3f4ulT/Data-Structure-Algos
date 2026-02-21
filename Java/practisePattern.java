61aa8c34630fc31a2bc9b90ffd8bc58cc78a5974
Shivansh Pandey <yashpandey.1307@gmail.com>
2026-02-21 14:55:56 +0530

    Practise Patterns for Libsys Interview

--- Java/practisePattern.java
+++ Java/practisePattern.java
@@ -1,7 +1,8 @@
 class pattern {
+  int i, j, k;
   void squareHollow(int n) {
-    for(int i = 0; i < n; i++) {
-      for(int j = 0; j < n; j++) {
+    for(i = 0; i < n; i++) {
+      for(j = 0; j < n; j++) {
         if(i == 0 || i == n - 1 || j == 0 || j == n - 1) {
           System.out.print("* ");
         } else {
@@ -12,34 +13,44 @@ class pattern {
     }
   }
   void numberTraingular(int n) {
-    for(int i = 1; i <= n; i++) {
+    for(i = 1; i <= n; i++) {
       //Inner loop for printing spaces
-      for(int j = 1; j <= n - i; j++) { 
+      for(j = 1; j <= n - i; j++) { 
         System.out.print(" ");
       }
       //Inner loop for printing numbers
-      for(int k = 1; k <= i; k++) {
+      for(k = 1; k <= i; k++) {
         System.out.print(i + " ");
       }
       System.out.println();
     }
   }
   void numberIncreasingPyramid(int n) {
-    for(int i = 1; i <= n; i++) {
-      for(int j = 1; j <= i; j++) {
+    for(i = 1; i <= n; i++) {
+      for(j = 1; j <= i; j++) {
         System.out.print(j + " ");
       }
       System.out.println();
     }
   }
   void numberIncreasingReversePyramid(int n) {
-    for(int i = 1; i <= n; i++) {
-      for(int j = 1; j <= n -i + 1; j++) {
+    for (i = n; i >= 1; i--) {
+      for (j = 1; j <= i; j++) {
         System.out.print(j + " ");
       }
       System.out.println();
     }
   } 
+  void numberChangingPyramid(int n) {
+    int num = 1;
+    for(i = 1; i <= n; i++) {
+      for(j = 1; j <= i; j++) {
+        System.out.print(num + " ");
+        num++;
+      }
+      System.out.println();
+    }
+  }
 }
 public class practisePattern {
   public static void main(String[] args) {
@@ -47,6 +58,7 @@ public class practisePattern {
     //p.squareHollow(5);
     //p.numberTraingular(5);
     //p.numberIncreasingPyramid(5);
-    p.numberIncreasingReversePyramid(5);
+    //p.numberIncreasingReversePyramid(5);
+    p.numberChangingPyramid(5); 
   }
 }
