dce7e982e4dab208f1c83e519826209ea7b492f1
Shivansh Pandey <yashpandey.1307@gmail.com>
2026-04-02 23:35:39 +0530

    afwa

--- Java/bitManipulation.java
+++ Java/bitManipulation.java
@@ -8,9 +8,25 @@ class bitManipulationProblems {
     }
     return binary.reverse().toString();
   }
+  public int BianryToDecimal(String binary) { //TC : O(n), where n is the length of the binary string. SC : O(1) for storing the decimal value.
+    int len = binary.length();  
+    int decimalValue = 0;
+    for(int i = len - 1; i >= 0; i--) {
+      int bit = binary.charAt(i) - '0'; // Convert character to integer (0 or 1)
+      decimalValue = decimalValue + bit * (int)Math.pow(2, len - 1 - i);
+    }
+    return decimalValue;
+  }
 }
 public class bitManipulation {
   public static void main(String[] args) {
+    bitManipulationProblems obj = new bitManipulationProblems();
+    int n = 10;
+    String binary = obj.IntegertoBinary(n);
+    System.out.println("Binary representation of " + n + " is: " + binary);
     
+    String binaryString = "1010";
+    int decimalValue = obj.BianryToDecimal(binaryString);
+    System.out.println("Decimal value of binary string " + binaryString + " is: " + decimalValue);
   }
 }
