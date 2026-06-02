677f9216eba854f01d924db1acc25c2f2e73ea05
Shivansh Pandey <yashpandey.1307@gmail.com>
2026-06-02 21:12:45 +0530

    metamorphsis

---
+++ Python/loops.py
@@ -0,0 +1,15 @@
+name = input("What is your name :- ")
+
+# WHILE LOOP
+# while name == "":
+#     print("You didn't enter a name.")
+#     name = input("What is your name :- ")
+# else:
+#     print(f"Wassup {name}!")
+
+
+# FOR LOOP
+for i in range(5):
+    if i == 2:
+        continue
+    print(f"i is currently {i}")
