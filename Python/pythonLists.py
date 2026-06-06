06927217fd2c23fd16c46baf62d2fb56a2dce401
Shivansh Pandey <yashpandey.1307@gmail.com>
2026-06-06 20:56:16 +0530

    study

--- Python/pythonLists.py
+++ Python/pythonLists.py
@@ -24,7 +24,7 @@ my_list = [1, 2, 2, 3, 4, 5, 5]
 print(my_list[0:])
 print(my_list[0:7])
 print(my_list[::-1])
-
+print()
 
 list1 = [5, 2, 9, 1, 5, 6]
 #SORTING LIST
@@ -35,34 +35,47 @@ list1.sort()
 print(f"Sorted list: {list1}")
 print()
 
+#FINDING LENGTH OF A LIST
+length = len(list1)
+print(f"Length of the list: {length}")
+print()
+
 #ADDING ELEMENTS IN LIST
 list1.append(10)
 print(f"List after adding element 10: {list1}")
+print()
 
 #COPYING A LIST
 list2 = list1.copy()
 print(f"Copied list: {list2}")
+print()
 
 #REMOVING ELEMENTS FROM A LIST
 list1.remove(5)  #removes first occurrence of 5
 print(f"List after removing element 5: {list1}")
+print()
 
 #COUNTING OCCURRENCES OF AN ELEMENT IN A LIST
 count = list1.count(5)
 print(f"Number of occurrences of 5 in the list: {count}")
+print()
 
 #INSERTING ELEMENTS IN A LIST
 list1.insert(2, 15)  #inserts 15 at index 2
 print(f"List after inserting element 15 at index 2: {list1}")
+print()
 
 #RETURINING THE INDEX OF AN ELEMENT IN A LIST   
 index = list1.index(9)
 print(f"Index of element 9 in the list: {index}")
+print()
 
 #REVERSING A LIST
 list1.reverse()
 print(f"Reversed list: {list1}")
+print()
 
 #CLEARING A LIST
 list1.clear()
 print(f"List after clearing: {list1}")
+print()
---
+++ Python/pythonSets.py
@@ -0,0 +1,5 @@
+#SETS : {} unordered, immutable, no duplicate values
+
+juice = {2, 2, 1, 3, 3, 4, 5, 6 ,7, 8, 9, 10}
+
+print(juice)  #duplicates will be removed and order is not guaranteed
