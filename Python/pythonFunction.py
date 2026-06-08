a941ac583f7c0e25328dbdc27cd7c07728b428b1
Shivansh Pandey <yashpandey.1307@gmail.com>
2026-06-09 04:03:35 +0530

    started functions

---
+++ Python/pythonFunction.py
@@ -0,0 +1,15 @@
+#FUNCTION = A block of resusable code that performs a specific task, takes some input, and may return an output.
+#place () after function name to invoke it
+
+# def greet(name):
+#     return f"Hello, {name}!"
+
+# user_name = input("Enter your name: ")
+# print(greet(user_name))
+
+#RETURN STATEMENT = Used to send a value back from a function to the caller. It also exits the function immediately.
+
+# def sum(a, b):
+#   return a + b
+
+# print(sum(3,2))
