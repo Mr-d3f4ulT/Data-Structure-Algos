346abdc1658b84962c5cc03d07d7d21cf699b84e
Shivansh Pandey <yashpandey.1307@gmail.com>
2026-06-10 16:39:11 +0530

    done with modules

---
+++ Python/modules.py
@@ -0,0 +1,12 @@
+#MODULES : A file containing code you want to include in your program
+#          use 'import' to include a module (built-in or your own)
+#          useful to break up a large program reusable seperate files
+
+# import math
+import math as m #using alias for math module
+from math import pi #importing specific feature from module
+
+print(pi)
+
+#To import a file.py in our current program make sure file and current file are in same folder
+#import file
