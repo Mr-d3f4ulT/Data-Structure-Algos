3e7b7134bd2331877d6b0d0b1d5578306c9ec358
Shivansh Pandey <yashpandey.1307@gmail.com>
2026-06-28 00:40:10 +0530

    maa xhudai ye topic bhenchod

---
+++ Python/decorator.py
@@ -0,0 +1,10 @@
+# DECORATOR : A function that extends the behavior of another function
+#             w/o modifying the base function
+#             Pass the base function as an argument to the decorator
+# A decorator is a function that adds or modifies the behavior of another function without changing its original code.
+# Think of it like adding toppings to a pizza 🍕:
+#    Pizza = Original function
+#    Cheese = Decorator
+#    Olives = Another decorator
+# The pizza is still the same, but its behavior (or appearance) has been enhanced.
+
